7019cabc9f5e004d07e8105966b88bacf5bc680e
Guillaume Leroi <leroi.g@gmail.com>
2017-05-17 00:32:32 +0200

    Element : Render to Element or IMDraw

--- elements.go
+++ elements.go
@@ -5,10 +5,29 @@ import (
 	"github.com/faiface/pixel/imdraw"
 )
 
-func Button(onClick func(), content func() *imdraw.IMDraw) *imdraw.IMDraw {
+var backend = make(map[Element]func())
+
+func Button(onClick func(), content Element) *ButtonElement {
 	// record onClick for Button region
-	r := content()
-	return r
+	button := &ButtonElement{
+		Content: content,
+		OnClick: onClick,
+	}
+	backend[button] = onClick
+	return button
+}
+
+type ButtonElement struct {
+	Content Element
+	OnClick func()
+}
+
+func (b *ButtonElement) Bounds() pixel.Rect {
+	return b.Content.Bounds()
+}
+
+func (b *ButtonElement) Render() interface{} {
+	return b.Content.Render()
 }
 
 type BoxElement struct {
@@ -16,24 +35,44 @@ type BoxElement struct {
 	Width, Height float64
 }
 
-func Box(x, y float64, width, height float64) BoxElement {
-	return BoxElement{
+func (b *BoxElement) Bounds() pixel.Rect {
+	return pixel.Rect{
+		Min: pixel.V(b.X, b.Y),
+		Max: pixel.V(b.X+b.Width, b.Y+b.Height),
+	}
+}
+
+type Element interface {
+	Bounds() pixel.Rect
+	Render() interface{}
+}
+
+func Box(x, y float64, width, height float64) *BoxElement {
+	return &BoxElement{
 		X: x, Y: y,
 		Width:  width,
 		Height: height,
 	}
 }
-func (b *BoxElement) Render() *imdraw.IMDraw {
+
+func (b *BoxElement) Render() interface{} {
 	imd := imdraw.New(nil)
+
+	foreground := pixel.RGB(1, 0, 0)
+	background := pixel.RGB(1, 1, 0)
+	borderThickness := 5.5
 
 	x := b.X
 	y := b.Y
 	width := b.Width
 	height := b.Height
 
-	foreground := pixel.RGB(1, 0, 0)
-	background := pixel.RGB(1, 1, 0)
-	borderThickness := 5.5
+	imd.Color(foreground)
+	imd.Push(pixel.V(x-borderThickness, y-borderThickness),
+		pixel.V(x+width+borderThickness, y-borderThickness),
+		pixel.V(x+width+borderThickness, y+height+borderThickness),
+		pixel.V(x-borderThickness, y+height+borderThickness))
+	imd.Polygon(0)
 
 	imd.Color(background)
 	imd.Push(pixel.V(x, y),
@@ -41,15 +80,6 @@ func (b *BoxElement) Render() *imdraw.IMDraw {
 		pixel.V(x+width, y+height),
 		pixel.V(x, y+height))
 	imd.Polygon(0)
-
-	imd.Color(foreground)
-	imd.Push(pixel.V(x, y),
-		pixel.V(x+width, y),
-		pixel.V(x+width, y+height),
-		pixel.V(x, y+height),
-		pixel.V(x, y))
-	imd.EndShape(imdraw.NoEndShape)
-	imd.Line(borderThickness)
 
 	return imd
 }
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:     "Pixel Rocks!",
+		Title:     "ui",
 		Bounds:    pixel.R(0, 0, 1024, 768),
 		Resizable: true,
 		VSync:     true,
@@ -18,11 +21,33 @@ func run() {
 		panic(err)
 	}
 
-	b := Box(50, 50, 150, 100)
-	imd := b.Render()
+	b := Button(func() {
+		log.Print("button clicked!")
+	}, Box(50, 50, 150, 100))
+
+	var result interface{} = b
+	done := false
+	for !done {
+		switch t := result.(type) {
+		case Element:
+			result = t.Render()
+		case *imdraw.IMDraw:
+			done = true
+		}
+	}
+	imd := result.(*imdraw.IMDraw)
 
 	for !win.Closed() {
-		win.Clear(colornames.Aquamarine)
+		if win.JustReleased(pixelgl.MouseButtonLeft) {
+			clickPosition := win.MousePosition()
+			for elt, handler := range backend {
+				if elt.Bounds().Contains(clickPosition) {
+					handler()
+				}
+			}
+		}
+
+		win.Clear(colornames.White)
 		imd.Draw(win)
 		win.Update()
 	}
